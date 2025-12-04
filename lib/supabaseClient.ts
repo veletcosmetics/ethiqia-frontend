15ad2df4aedba6460f59ad9f4698102b5fdd7e49
veletcosmetics <veletcosmetics@gmail.com>
2025-12-04 18:01:10 +0100

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,9 @@
-// lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
+"use client";
+
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// Cliente para usar en componentes de React (navegador)
+export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
