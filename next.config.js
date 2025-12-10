9fb7d1135cc8d1facff10cddf9dde735ade6b28c
veletcosmetics <veletcosmetics@gmail.com>
2025-12-10 12:45:07 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gvmibmzvbsggdvbxiqjk.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
 };
-module.exports = nextConfig;
+
+export default nextConfig;
